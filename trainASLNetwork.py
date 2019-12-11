4685cf2e8d2ed1bc9f68b740edac81ea2a2066d8
dhruv singal <dhruvsingalabc@gmail.com>
2019-12-10 23:41:12 -0800

    testing simpler network without image augmentation generator

--- trainASLNetwork.py
+++ trainASLNetwork.py
@@ -4,9 +4,13 @@ import tensorflow as tf
 #%matplotlib inline
 import os
 import cv2
+from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.models import Sequential
 
 train_dir = "./asl_alphabet_train/asl_alphabet_train"
 eval_dir = "./asl_alphabet_test/asl_alphabet_test"
+
 
 #Helper function to load images from given directories
 def load_images(directory):
@@ -20,44 +24,34 @@ def load_images(directory):
             labels.append(idx)
     images = np.array(images)
     labels = np.array(labels)
-    return(images, labels)
-import keras
+    return (images, labels)
+
+
+print("now loading images")
 
 uniq_labels = sorted(os.listdir(train_dir))
-images, labels = load_images(directory = train_dir)
-
+images, labels = load_images(directory=train_dir)
+X_eval, y_eval = None, None
 if uniq_labels == sorted(os.listdir(eval_dir)):
-    X_eval, y_eval = load_images(directory = eval_dir)
+    X_eval, y_eval = load_images(directory=eval_dir)
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size = 0.1, stratify = labels)
+X_train, X_test, y_train, y_test = train_test_split(images,
+                                                    labels,
+                                                    test_size=0.1,
+                                                    stratify=labels)
 
 n = len(uniq_labels)
 train_n = len(X_train)
 test_n = len(X_test)
 
 print("Total number of symbols: ", n)
-print("Number of training images: " , train_n)
+print("Number of training images: ", train_n)
 print("Number of testing images: ", test_n)
 
 eval_n = len(X_eval)
 print("Number of evaluation images: ", eval_n)
-
-#Helper function to print images
-def print_images(image_list):
-    n = int(len(image_list) / len(uniq_labels))
-    cols = 8
-    rows = 4
-    fig = plt.figure(figsize = (24, 12))
-
-    for i in range(len(uniq_labels)):
-        ax = plt.subplot(rows, cols, i + 1)
-        plt.imshow(image_list[int(n*i)])
-        plt.title(uniq_labels[i])
-        ax.title.set_fontsize(20)
-        ax.axis('off')
-    plt.show()
 
 y_train_in = y_train.argsort()
 y_train = y_train[y_train_in]
@@ -70,36 +64,42 @@ y_test = keras.utils.to_categorical(y_test)
 y_eval = keras.utils.to_categorical(y_eval)
 print(y_train[0])
 print(len(y_train[0]))
-X_train = X_train.astype('float32')/255.0
-X_test = X_test.astype('float32')/255.0
-X_eval = X_eval.astype('float32')/255.0
-
-from keras.layers import Conv2D, Dense, Dropout, Flatten
-from keras.layers import Flatten, Dense
-from keras.models import Sequential
+X_train = X_train.astype('float32') / 255.0
+X_test = X_test.astype('float32') / 255.0
+X_eval = X_eval.astype('float32') / 255.0
 
 model = Sequential()
-model.add(Conv2D(filters = 64, kernel_size = 5, padding = 'same', activation = 'relu', 
-                 input_shape = (64, 64, 3)))
-model.add(Conv2D(filters = 64, kernel_size = 5, padding = 'same', activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (4, 4)))
+model.add(
+    Conv2D(filters=64,
+           kernel_size=5,
+           padding='same',
+           activation='relu',
+           input_shape=(64, 64, 3)))
+model.add(Conv2D(filters=64, kernel_size=5, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(4, 4)))
 model.add(Dropout(0.5))
-model.add(Conv2D(filters = 128 , kernel_size = 5, padding = 'same', activation = 'relu'))
-model.add(Conv2D(filters = 128 , kernel_size = 5, padding = 'same', activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (4, 4)))
+model.add(Conv2D(filters=128, kernel_size=5, padding='same',
+                 activation='relu'))
+model.add(Conv2D(filters=128, kernel_size=5, padding='same',
+                 activation='relu'))
+model.add(MaxPooling2D(pool_size=(4, 4)))
 model.add(Dropout(0.5))
-model.add(Conv2D(filters = 256 , kernel_size = 5, padding = 'same', activation = 'relu'))
+model.add(Conv2D(filters=256, kernel_size=5, padding='same',
+                 activation='relu'))
 model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(29, activation='softmax'))
 
 model.summary()
-model.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-hist = model.fit(X_train, y_train, epochs = 5, batch_size = 64)
-score = model.evaluate(x = X_test, y = y_test, verbose = 0)
-print('Accuracy for test images:', round(score[1]*100, 3), '%')
-score = model.evaluate(x = X_eval, y = y_eval, verbose = 0)
-print('Accuracy for evaluation images:', round(score[1]*100, 3), '%')
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+hist = model.fit(X_train, y_train, epochs=5, batch_size=64)
+model.save('aslClassifier.h5')
+score = model.evaluate(x=X_test, y=y_test, verbose=0)
+print('Accuracy for test images:', round(score[1] * 100, 3), '%')
+score = model.evaluate(x=X_eval, y=y_eval, verbose=0)
+print('Accuracy for evaluation images:', round(score[1] * 100, 3), '%')
 
-y_eval_pred = model.predict(X_eval, batch_size = 64, verbose = 0)
+y_eval_pred = model.predict(X_eval, batch_size=64, verbose=0)
 print(y_eval_pred)
